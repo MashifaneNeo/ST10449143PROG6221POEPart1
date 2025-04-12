63489cd909b02c3a74fe14ca9fc7e90979f2f1ef
MashifaneNeo <ST10449143@imconnect.edu.za>
2025-04-12 18:01:06 +0200

    Add project files.

---
+++ ST10449143PROG6221POEPart1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ST10449143PROG6221POEPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
